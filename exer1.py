3b716b65c21eccbe2e603fc9414129a13eb5bfa5
StellaWava <40366457+StellaWava@users.noreply.github.com>
2021-06-18 15:34:23 -0400

    Add files via upload

---
+++ exer1.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ice_cream = 4
+
+# sleeping_rating = 9
+
+# first_name = input('First Name')
+# print (first_name, 'it is.')
+
+# last_name = input('Last Name')
+# print (last_name, 'it is.')
+
+# my_name = first_name + last_name ##not certain of how to embed the spacing.
+# print(my_name, 'is my name')
+
+# ## Problem 3 - A bit of math
+
+# happiness_rating = (ice_cream + sleeping_rating)/2
+# print(happiness_rating)
+
+# ## Problem 4 - Checking data types
+
+# type(ice_cream)
+
+# type(first_name)
+
+# type(happiness_rating)
+
+# ## Problem 5 Displaying text on the screen
+
+# In[24]:
+
+
+print('My name is', my_name, 'and I give eating ice cream a score of', ice_cream , 'out of 10.')
+print('I am', my_name, 'and my sleeping enjoyment rating is', sleeping_rating , 'out of 10.')
+print('Based on factors above, my happiness rating is', happiness_rating, 'out of 10.')
+
+
+# In[ ]:
+
+
+
+
